87c749046f33ea10d854f78fe68e5baf380918e2
LorentDeMor <116681713+LorentDeMor@users.noreply.github.com>
2023-07-26 16:14:32 +0300

    Add files via upload
    Signed-off-by: LorentDeMor <116681713+LorentDeMor@users.noreply.github.com>

--- app1.js
+++ app1.js
@@ -11,3 +11,12 @@ zad1.onclick = function() {
     cons2 = cons1  + " Второй вывод в консоль: "+(a+' '+b+' '+c1)
     vivodZad1.innerHTML = cons2
 }
+
+const zad3 = document.getElementById('zad3')
+const but3 = document.getElementById('but3')
+const inp3 = document.getElementById('price')
+const inp31 = document.getElementById('quan')
+but3.onclick = function() {
+    var productQuantity = inp3.value * inp31.value
+    zad3.textContent = "Стоимость Ваших товаров: "+productQuantity+" рублей!"
+}
